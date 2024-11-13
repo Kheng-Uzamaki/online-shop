d46782bd26273fe2ece42c738bd0fa2677c747e5
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-11-13 12:51:36 +0700

    add .env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to Kheng-Shop API");
@@ -13,9 +15,9 @@ app.get("/api/products", (req, res) => {
 });
 
 app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
