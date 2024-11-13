8df7d6a2458113c278dfac932368e814e5d58be4
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-11-13 11:00:22 +0700

    display product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
-
       </main>
       <Footer />
     </>
