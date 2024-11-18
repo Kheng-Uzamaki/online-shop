ab0931bd02f37fa44d245db7374e088c43791c84
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-11-18 15:58:51 +0700

    user Profile update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -15,7 +15,7 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
       console.log(error);
