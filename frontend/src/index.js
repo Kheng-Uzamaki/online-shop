77c204abed54390ea66af126bb742aacce6122dc
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-11-19 10:24:55 +0700

    Login Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import { Provider } from "react-redux";
 import store from "./store";
+import LoginScreen from "./screens/LoginScreen";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
