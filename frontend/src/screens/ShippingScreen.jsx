d7836a979526b510679ac09fd8c56772e518786b
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-11-20 10:22:48 +0700

    checkout process step

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import FormContainer from "../components/FormContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckOutStep from "../components/CheckOutStep";
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -25,6 +26,7 @@ const ShippingScreen = () => {
   };
   return (
     <FormContainer>
+      <CheckOutStep step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
